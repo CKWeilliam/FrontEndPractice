972bcb13412a059ffad176d9f7c7d03dd94c246a
SUPERMICRO\WilliamChang <WilliamChang@supermicro.com>
2023-12-25 17:29:13 +0800

    Add more mark file

--- src/pages/Home/sbom-reports/RealMarkFile.js
+++ src/pages/Home/sbom-reports/RealMarkFile.js
@@ -1,7 +1,7 @@
 export const MarkData = [
     {
-        'totalItems': 10,
-        'data_count_per_page': 10,
+        'totalItems': 25,
+        'data_count_per_page': 25,
         'current_page': 1,
         'data': [
             {
@@ -61,6 +61,96 @@ export const MarkData = [
             {
                 '_id': '65781e90531ce94c71393a12',
                 'file_name': 'Cybellum_ISO_21434_WP-05-03_SBOM-X10DRU2.427_20220428.bin-202204281519-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e90531ce94c71393a14',
+                'file_name': 'Cybellum_Licensing-X10DRU2.427_20220428.bin-202204281519-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e90531ce94c71393a16',
+                'file_name': 'Cybellum_Vulnerability_Management_X10DRU2.427_20220428.bin_202204281519-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e90531ce94c71393a19',
+                'file_name': 'Cybellum_Cryptography-X10DRT2.427-202204281528-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e90531ce94c71393a1a',
+                'file_name': 'Cybellum_Full_Asset-X10DRT2.427-202204281528-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e90531ce94c71393a1b',
+                'file_name': 'Cybellum_ISO_21434_WP-05-03_SBOM-X10DRT2.427-202204281528-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e90531ce94c71393a1d',
+                'file_name': 'Cybellum_Licensing-X10DRT2.427-202204281528-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e90531ce94c71393a1f',
+                'file_name': 'Cybellum_Vulnerability_Management_X10DRT2.427_202204281528-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e91531ce94c71393a5b',
+                'file_name': 'Cybellum_Cryptography-9k1_51c.bin-202204281604-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e91531ce94c71393a5c',
+                'file_name': 'Cybellum_Full_Asset-9k1_51c.bin-202204281604-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e91531ce94c71393a5d',
+                'file_name': 'Cybellum_ISO_21434_WP-05-03_SBOM-9k1_51c.bin-202204281604-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e91531ce94c71393a5f',
+                'file_name': 'Cybellum_Licensing-9k1_51c.bin-202204281604-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e91531ce94c71393a61',
+                'file_name': 'Cybellum_Vulnerability_Management_9k1_51c.bin_202204281604-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e91531ce94c71393a67',
+                'file_name': 'Cybellum_Cryptography-9k1_51c.zip-202208011330-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e91531ce94c71393a68',
+                'file_name': 'Cybellum_Full_Asset-9k1_51c.zip-202208011330-AUTOTEST.xlsx',
+                'file_type': 'xlsx',
+                'rom_type': 'ipmi'
+            },
+            {
+                '_id': '65781e91531ce94c71393a69',
+                'file_name': 'Cybellum_ISO_21434_WP-05-03_SBOM-9k1_51c.zip-202208011330-AUTOTEST.xlsx',
                 'file_type': 'xlsx',
                 'rom_type': 'ipmi'
             }
