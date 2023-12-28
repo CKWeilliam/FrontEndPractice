158a2f28fd71fcc1f20a664c8b485f6d6460ddc5
SUPERMICRO\WilliamChang <WilliamChang@supermicro.com>
2023-12-28 17:56:44 +0800

    新增原搜尋分頁方案版本支線

--- src/pages/Home/sbom-reports/SbomReportSearch.jsx
+++ src/pages/Home/sbom-reports/SbomReportSearch.jsx
@@ -12,30 +12,40 @@ const SbomReports = () => {
     const [isLoading, setIsLoading] = useState(false)
     const [showResult, setShowResult] = useState(false)
     const [resultCount, setResultCount] = useState(0)
-    const [searchResultList, setSearchResultList] = useState([])
-    const [result, serResult] = useState([]);
+    const [searchResultList, setSearchResultList] = useState([]) 
+    const [result, serResult] = useState([])
     const [selectedItems, setSelectedItems] = useState([])
     const [sortOrder, setSortOrder] = useState('asc') // 用來排序的方法 升、降
     const [sortBy, setSortBy] = useState('id')   // 排序的條件
-
-    const [page, setPage] = useState(1);
+    const [page, setPage] = useState(1)
 
     const [SearchData, setSearchData] = useState({
-        page: '1',
-        limit: '10',
         sutType: '',
         fileType: '',
-        fileName: '',
+        fileName: null,
     })
 
     /**
      * 偵測isLoading 與 SearchData
      */
     useEffect(() => {
-        setResultCount(result.totalItems > 0 ? result.totalItems : 0)
+        setResultCount(searchResultList.length)
         // console.log('Result count updated:', resultCount)
         // console.log('SearchData updated:', SearchData)
-    }, [SearchData, isLoading]) 
+    }, [SearchData, isLoading])
+
+    useEffect(() => {
+        // console.log('SearchData has changed:', SearchData)
+        console.log('searchResultList has changed : ', JSON.stringify(searchResultList.slice(0, 20)))
+    }, [SearchData,searchResultList])
+    /**
+     * 偵測當排序條件與順序發生變動時 呼叫排序方法
+     */
+    // useEffect(() => {
+    //     if (searchResultList.length > 0) {
+    //         setSearchResultList(sortResults(searchResultList, sortBy, sortOrder))
+    //     }
+    // }, [sortBy, sortOrder])  
 
     /**
      * 處理input資料事件
@@ -53,59 +63,80 @@ const SbomReports = () => {
      * 處理全選checkbox後的file_id
      * @param {*} e 
      */
-    const handleSelectAll = (e) => {
-        if (e.target.checked) {
-            setSelectedItems(searchResultList.map(item => item._id))
+    const handleSelectAll = (e) => { 
+        if (e.target.checked) { 
+            setSelectedItems(searchResultList.map(item => item.file_id)) 
         } else {
             setSelectedItems([])
         }
     }
+    const simulateLoading = (options = {}) => {
+        const { delay = 1000, ...stateUpdates } = options
+
+        setTimeout(() => {
+            Object.values(stateUpdates).forEach(updateFunc => {
+                if (typeof updateFunc === 'function') {
+                    updateFunc()
+                }
+            })
+        }, delay)
+    }
 
     /**
      * 處理搜尋結果
-     * 1.If the backend has changed the Body's key, modify it here.
-     * 2.map searchData key names to jsonDataForSearch key names
-     * 3.
      * @param {*} searchData 
      */
     const handleSearchResult = async (searchData) => {
-        console.log('測試 MarkData[0].totalItems: ', MarkData[0].totalItems)
-        console.log('searchData.page:', searchData);
+        // console.log('測試測試測試',MarkData[0].totalItems)
         setIsLoading(true)
 
-        const keyMapping = {
-            'page': 'page',
-            'limit': 'limit',
-            'sutType': 'rom_type',
-            'fileType': 'file_type',
-            'fileName': 'file_name'
-        }
 
+        // If the backend has changed the Body's key, modify it here.
+        const keyMapping = {
+            'fileName': 'rom_name',
+            'sutType': 'rom_type',
+            'fileType': 'file_type'
+        }
+        // Map searchData key names to jsonDataForSearch key names
         const jsonDataForSearch = Object.keys(searchData).reduce((acc, key) => {
             const newKey = keyMapping[key] || key
-            acc[newKey] = searchData[key]
+            // Check if the key has a corresponding value in searchData
+            if (searchData[key] !== undefined && searchData[key] !== null && searchData[key] !== '') {
+                // transformedValue
+                const transformedValue = (searchData[key] === 'BMC') ? 'ipmi' : searchData[key].toLowerCase()
+                acc[newKey] = transformedValue
+            }
+            
             return acc
         }, {})
 
+
         try {
-            // console.log(JSON.stringify('傳入格式 :', jsonDataForSearch)) // 之前寫法
-            console.log('傳入格式 :', JSON.stringify(jsonDataForSearch)) // 正確寫法
+            console.log('傳入格式 :', JSON.stringify(jsonDataForSearch))
             // Search data from API
-            // const searchResult = await getReportSearch()
-            // setSearchResultList(searchResult.data)
-            // console.log('searchResultList', searchResultList) 
-            console.log('測試 MarkData[0].data: ', MarkData[0].data)
-            setSearchResultList(MarkData[0].data);
-            serResult(MarkData[0]);
+            // const response = await getReportSearch(jsonDataForSearch)
+            // const searchResultToFlattenedData = response.data.data
+            // // console.log('searchResultToFlattenedData : ', searchResultToFlattenedData)
+            // const searchFlattenedData = searchResultToFlattenedData.map(item => ({
+            //     '_id': item.reports._id,
+            //     'file_name': item.reports.file_name,
+            //     'file_type': item.reports.file_type,
+            //     'rom_type': item.rom_type
+            // }))
+            // // console.log('searchFlattenedData : ', searchFlattenedData)
+            // setSearchResultList(searchFlattenedData)
+            
+            setSearchResultList(MarkData[0].data)
             console.log('資料', MarkData[0].data)
 
         } catch (error) {
-            console.log('Failing post')
+
+            console.log('Failing post', error)
         } finally {
             simulateLoading({
-                setIsLoading: () => setIsLoading(false),
-                setShowResult: () => setShowResult(true),
-                delay: 1000
+                setIsLoading: () => setIsLoading(false), 
+                setShowResult: () => setShowResult(true), 
+                // delay: 1000
             })
         }
 
@@ -114,12 +145,20 @@ const SbomReports = () => {
     /**
      * 標題欄位
      */
-    const VISIBLE_FIELDS = ['File Name', 'SUT Type', 'File Type', 'File Action']
+    const VISIBLE_FIELDS = ['File Name', 'Single Download'] 
+
+    /**
+     * 排序的條件
+     */
+    // const sortingOptions = [
+    //     { label: 'Part No', value: 'part_no' },
+    //     { label: 'Part Type', value: 'part_type' }
+    // ]
 
     return (
         <div>
             <div className="m-4 border-b-2">
-                <h1 className="text-2xl font-bold text-start mb-4 dark:border-white w-full flex items-center">Search Page
+                <h1 className="text-2xl font-bold text-start mb-4 dark:border-white w-full flex items-center">SBOM Reports
                     {/* <About /> */}
                 </h1>
             </div>
@@ -150,22 +189,24 @@ const SbomReports = () => {
                     {/* 新增的排序按鈕與選項 */}
                     <div className='flex justify-between items-center p-4'>
                         <h2 className='text-xl font-bold'>Found Results： {resultCount}</h2>
+                        {/* <SortResult sortOrder={sortOrder} setSortOrder={setSortOrder} sortBy={sortBy} setSortBy={setSortBy}/> */}
                     </div>
 
                     {/* 新增的標頭欄位 */}
                     <div className='flex items-center justify-between p-4 bg-gray-400 rounded'>
                         <span className='flex items-center justify-center font-bold border-gray-300 p-2 w-1/6'>
-                            <Checkboxes
+                            {/* <Checkboxes
                                 onChange={handleSelectAll}
                                 checked={selectedItems.length === searchResultList.length && searchResultList.length > 0}
                                 indeterminate={selectedItems.length > 0 && selectedItems.length < searchResultList.length}
-                            />Select All
+                            />Select All */}
+                            Select File
                         </span>
                         {VISIBLE_FIELDS.map((field, index) => (
                             <span key={index} className='font-bold border border-gray-400 p-4 text-center w-1/6'>
                                 {field}
                             </span>
-                        ))}
+                        ))} 
                     </div>
 
                     {isLoading ? <Loading /> : (
--- src/pages/Home/sbom-reports/SearchResult.jsx
+++ src/pages/Home/sbom-reports/SearchResult.jsx
@@ -4,38 +4,22 @@ import Stack from '@mui/material/Stack'
 import Checkboxes from './mui-components/CheckBox'
 
 const SearchResult = ({ resultData, selectedCheckBox, result, setResultCount, handleSearchResult, page, setPage }) => {
-    const [selectedItems, setSelectedItems] = useState([]);
+    const [selectedItems, setSelectedItems] = useState([])
     // const [page, setPage] = useState(1);
 
     useEffect(() => {
         setSelectedItems(selectedCheckBox)
     }, [selectedCheckBox])
 
-    const handleBulkDownload = () => {
+    const handleBulkDownload = async (fileId) => {
+        const ids = ['65781e5d531ce94c71393a04', '65781e86531ce94c71393a05', '65781e90531ce94c71393a07', '65781e90531ce94c71393a08', '65781e90531ce94c71393a09']
+        const queryParams = `_ids=${ids.join('&_ids=')}`
 
-        selectedItems.forEach(fileId => {
-            console.log(`一次下載所有檔案：${fileId}`)
-        })
-    }
-
-    const handleDownload = async (fileId) => {
-        //Test Download function
-        console.log(`下載檔案：${fileId}`)
-
-        // Download File from API
-        // try {
-        //     const response = await GetDownloadThirdPartyPackage(fileId)
-        //     console.log(response)
-
-        //     if (!response.ok) {
-
-        //     }
-
-        //     // 取得檔案名稱，先假設後端在 response headers 中提供檔案名稱，要跟kevin討論能不能在header放資訊
-        //     const fileName = response.headers['content-disposition'].split('filename=')[1]
-        //     // 'filename= TEST.pdf'
+        try{
+        //     const response = await getDownloadSbomReportMultiFiles(queryParams)
+        //     const fileName = 'TestMultiFiles'
         //     // 將檔案內容設置到 Blob 中，並設定檔案類型
-        //     const blob = new Blob([response.data], { type: response.headers['content-type'] })
+        //     const blob = new Blob([response], { type: 'application/zip'})
         //     const link = document.createElement('a')
         //     link.href = window.URL.createObjectURL(blob)
 
@@ -44,13 +28,47 @@ const SearchResult = ({ resultData, selectedCheckBox, result, setResultCount, ha
         //     document.body.appendChild(link)
         //     link.click()
         //     document.body.removeChild(link)
-        // } catch (error) {
-        //     console.error('下載檔案失敗', error)
-        // }
+        // // selectedItems.forEach(fileId => {
+            console.log(`一次下載所有檔案：${fileId}`)
+        // // })
+        } catch (error) {
+            console.error('下載多檔案壓縮檔失敗', error)
+            
+        }
+    }
+
+    const handleDownload = async (fileId) => {
+        //Test Download function
+        
+        const _id = '65781e5d531ce94c71393a04'
+
+        // Download File from API
+        try {
+            console.log(`下載檔案：${fileId}`)
+        //     const response = await getDownloadSbomReportSingleFile(_id)
+        //     console.log(response)
+        //     const fileName = 'Test'
+        //     // const fileName = response.headers['content-disposition'].split('filename=')[1]
+
+        //     // 將檔案內容設置到 Blob 中，並設定檔案類型
+        //     // const blob = new Blob([response], { type: response.headers['content-type'] })
+        //     const blob = new Blob([response], { type: 'application/vnd.ms-excel;charset=utf-8;'})
+        //     const link = document.createElement('a')
+        //     link.href = window.URL.createObjectURL(blob)
+
+        //     // 連結的下載檔案的名稱
+        //     link.download = `${fileName}.xlsx`
+        //     document.body.appendChild(link)
+        //     setTimeout(() => {
+        //         link.click()
+        //         document.body.removeChild(link)
+        //     }, 3000)
+        } catch (error) {
+            console.error('下載檔案失敗', error)
+        }
     }
 
     const Items = ({ currentItems }) => {
-        // console.log('currentItems', currentItems);
 
         /**
          * 處理checkbox file_id 改變的function，單選打勾時，會setSelectedItems
@@ -83,15 +101,15 @@ const SearchResult = ({ resultData, selectedCheckBox, result, setResultCount, ha
                                 />
                             </span>
                         </div>
-                        <div className="flex justify-center items-center p-4 text-center w-1/6 text-xs ">
+                        <div className="flex justify-center items-center p-4 text-center w-4/6 text-xs ">
                             <span className="" style={{ maxWidth: '100%', overflowWrap: 'break-word' }}>{item.file_name}</span>
                         </div>
-                        <div className="flex justify-center items-center p-4 text-center w-1/6">
+                        {/* <div className="flex justify-center items-center p-4 text-center w-1/6">
                             <span className="">{item.rom_type}</span>
                         </div>
                         <div className="flex justify-center items-center p-4 text-center w-1/6">
                             <span className="">{item.file_type}</span>
-                        </div>
+                        </div> */}
                         <div className="flex justify-center items-center p-4 text-center w-1/6">
                             <button
                                 className="button button-success h-8"
@@ -108,41 +126,51 @@ const SearchResult = ({ resultData, selectedCheckBox, result, setResultCount, ha
 
 
     const PaginatedItems = ({ page, setPage, items, result }) => {
-        console.log('PaginatedItems init: items', items);
+        // console.log('PaginatedItems init: items', items)
         // const [page, setPage] = useState(1);
         const itemsPerPage = 10
 
         const handlePageChange = (event, value) => {
-            console.log('PaginatedItems: handlePageChange s value:', value);
-            console.log('e.target:', event.target);
+            console.log('PaginatedItems: handlePageChange s value:', value)
+            console.log('e.target:', event.target)
             // setPage(value);
             
-            console.log('After setPage:', value);
+            console.log('After setPage:', value)
 
-            const pageSearchData = {
-                page: value,
-                limit: 10,
-                sutType: items[0].rom_type,
-                fileType: items[0].file_type,
-                fileName: items[0].file_name,
-            };
+            // const pageSearchData = {
+            //     page: value,
+            //     limit: 10,
+            //     sutType: items[0].rom_type,
+            //     fileType: items[0].file_type,
+            //     fileName: items[0].file_name,
+            // }
 
-            handleSearchResult(pageSearchData);
+            // handleSearchResult(pageSearchData)
 
-            setPage(value);
+            setPage(value)
         }
 
-        // const startIndex = (page - 1) * itemsPerPage  // 0
-        // const endIndex = startIndex + itemsPerPage  // 0 + 10
-        // const currentItems = items.slice(startIndex, endIndex);
-        console.log('PaginatedItems: items', items);
-        console.log('PaginatedItems: result:', result);
-        console.log('PaginatedItems: result totalItems:', result.totalItems);
-        const pageCount = Math.ceil((result.totalItems > 0 ? result.totalItems : 1) / itemsPerPage);
-
+        const startIndex = (page - 1) * itemsPerPage  // 0
+        const endIndex = startIndex + itemsPerPage  // 0 + 10
+        const currentItems = items.slice(startIndex, endIndex)
+        // console.log('PaginatedItems: items', items)
+        // console.log('PaginatedItems: result:', result)
+        // console.log('PaginatedItems: result totalItems:', result.totalItems)
+        // const pageCount = Math.ceil((result.totalItems > 0 ? result.totalItems : 1) / itemsPerPage)
+        const pageCount = Math.ceil( items.length / itemsPerPage)
         return (
             <>
-                <Items currentItems={items} />
+                <Items currentItems={currentItems} />
+                {selectedItems.length > 0 && (
+                    <div className="flex justify-center p-4">
+                        <button
+                            className="button button-primary h-10 px-5"
+                            onClick={handleBulkDownload}
+                        >
+                                 Download All Selected
+                        </button>
+                    </div>
+                )}
                 <Stack spacing={2} justifyContent="center" alignItems="center">
                     <Pagination
                         count={pageCount}
@@ -167,21 +195,14 @@ const SearchResult = ({ resultData, selectedCheckBox, result, setResultCount, ha
             {
                 resultData.length === 0 ? (
                     <div className="flex h-full items-center justify-center">
-                        <p className="text-xl font-medium text-red-500">Sorry, No Search Result</p>
+                        <p className="text-xl font-medium text-red-500">No Result is Found.</p>
                     </div>
                 ) : (
+
                     <>
+
                         <PaginatedItems  page={page} setPage={setPage} items={resultData} result={result} />
-                        {selectedItems.length > 0 && (
-                            <div className="flex justify-center p-4">
-                                <button
-                                    className="button button-primary h-10 px-5"
-                                    onClick={handleBulkDownload}
-                                >
-                                    Download All Selected
-                                </button>
-                            </div>
-                        )}
+
                     </>
                 )
             }
